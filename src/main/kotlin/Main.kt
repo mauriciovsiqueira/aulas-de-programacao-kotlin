a2ccbd9bfa53a8a028f2e36759bd4304001bc9f0
Mauricio Siqueira <mauriciovsiqueira@outlook.com.br>
2026-07-02 13:22:28 -0300

    Novas aulas.

--- src/main/kotlin/Main.kt
+++
@@ -1,5 +0,0 @@
-// Ponto de entrada, ele executa o código.
-fun main() {
-    // Imprimi no console.
-    println("Olá Mundo")
-}//
---
+++ src/main/kotlin/aulas/conceitosIniciais/Teste.kt
@@ -0,0 +1,12 @@
+package aulas.conceitosIniciais
+
+// -> Comentários de uma linha.
+/*
+* Comentário de varias linhas.
+*/
+
+// Ponto de entrada que executa o código.
+fun main() {
+    // Imprimi no console.
+    println("Olá Mundo")
+}
